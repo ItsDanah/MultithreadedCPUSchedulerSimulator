fa970470cb3df5ce9512cc3f167e5d341dc09c98
Danah <itsdanahsaleh@gmail.com>
2026-05-08 14:42:39 +0300

    Update Memory.java

--- Memory.java
+++ Memory.java
@@ -1,26 +1,28 @@
-package os;
+// ─────────────── This class simulates the RAM ─────────────────────
 
 public class Memory {
-	public final int MAX_MEMORY = 2048;
-	private int availableMemory ;
-	
+	public final int MAX_MEMORY = 2048; // size of memory
+	private int availableMemory ; // free memory
+
 	public Memory() {
-		this.availableMemory= MAX_MEMORY;
-		
+		this.availableMemory= MAX_MEMORY; // initially memory is fully available
 	}
 
+	// ── memory allocation and deallocation ──────────────────────────────────
 	public  synchronized boolean allocateMemory(int size ) {
-		if (availableMemory >= size) {
-			availableMemory = availableMemory -size;
-			return true;
+		if (availableMemory >= size) { 
+			availableMemory = availableMemory -size; // reserves memory
+			return true; // memory successfully allocated
 		}
 		System.out.println("No Available Memory");
-		return false ;
+		return false ; // failed to allocate memory
 	}
+
 	public synchronized void deallocateMemory( int size ) {
-		availableMemory = availableMemory + size ;
-		
+		availableMemory = availableMemory + size ; // frees memory after process termination
 	}
+
+	// ── setters and getters ──────────────────────────────────
 	public synchronized int getavailableMemory() {
 		return availableMemory;
 	}
